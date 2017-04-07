28990bc8a96159dd9eefcdd77664082a7b1afa30
Leon Zhu <leonzhu42@hotmail.com>
2017-04-07 21:47:27 +0800

    Fix mixed tabs and spaces
    transform all tabs to spaces

--- direction_detector.cpp
+++ direction_detector.cpp
@@ -72,101 +72,101 @@ void findVanishingPoint(int = 0, void* = 0) {
     }
 
     // Double2 vp = vanishingPoint(lines);
-	// Step 1
-	std::vector<size_t> ind_filtered_lines;
-	for (size_t i = 0; i < lines.size(); ++i)
-		if (!(lines[i][0] == lines[i][2] ||
-			abs(lineSlope(lines[i])) < MIN_SLOPE_THRESHOLD ||
-			abs(lineSlope(lines[i])) > MAX_SLOPE_THRESHOLD))
-			ind_filtered_lines.push_back(i);
+    // Step 1
+    std::vector<size_t> ind_filtered_lines;
+    for (size_t i = 0; i < lines.size(); ++i)
+        if (!(lines[i][0] == lines[i][2] ||
+            abs(lineSlope(lines[i])) < MIN_SLOPE_THRESHOLD ||
+            abs(lineSlope(lines[i])) > MAX_SLOPE_THRESHOLD))
+            ind_filtered_lines.push_back(i);
 
-	// Sketch the filtered lines on color_dst
-	std::cout << "Filtered lines: " << ind_filtered_lines.size() << std::endl;
-	for (size_t i = 0; i < ind_filtered_lines.size(); i++) {
-		line(color_dst, Point(lines[ind_filtered_lines[i]][0], lines[ind_filtered_lines[i]][1]),
-			Point(lines[ind_filtered_lines[i]][2], lines[ind_filtered_lines[i]][3]), Scalar(255, 0, 0), 3, 8);
-	}
+    // Sketch the filtered lines on color_dst
+    std::cout << "Filtered lines: " << ind_filtered_lines.size() << std::endl;
+    for (size_t i = 0; i < ind_filtered_lines.size(); i++) {
+        line(color_dst, Point(lines[ind_filtered_lines[i]][0], lines[ind_filtered_lines[i]][1]),
+            Point(lines[ind_filtered_lines[i]][2], lines[ind_filtered_lines[i]][3]), Scalar(255, 0, 0), 3, 8);
+    }
 
-	// Step 2 & 3
-	std::vector<Double2> vps;
-	std::vector<Size_t2> ind_vps_lines;
-	for (size_t i = 0; i < ind_filtered_lines.size(); ++i)
-		for (size_t j = 0; j < i; ++j) {
-			Vec4i line1 = lines[ind_filtered_lines[i]];
-			Vec4i line2 = lines[ind_filtered_lines[j]];
-			Double2 vp = linesIntersect(line1, line2);
-			if (!intersectIsPillar(line1, line2, vp)) {
-				vps.push_back(vp);
-				ind_vps_lines.push_back(Size_t2(j, i));
-			}
-		}
+    // Step 2 & 3
+    std::vector<Double2> vps;
+    std::vector<Size_t2> ind_vps_lines;
+    for (size_t i = 0; i < ind_filtered_lines.size(); ++i)
+        for (size_t j = 0; j < i; ++j) {
+            Vec4i line1 = lines[ind_filtered_lines[i]];
+            Vec4i line2 = lines[ind_filtered_lines[j]];
+            Double2 vp = linesIntersect(line1, line2);
+            if (!intersectIsPillar(line1, line2, vp)) {
+                vps.push_back(vp);
+                ind_vps_lines.push_back(Size_t2(j, i));
+            }
+        }
 
-	// Mark the intersections of filtered lines
-	for (size_t i = 0; i < vps.size(); ++i) {
-		Point pt = Point(int(vps[i].c[0]), int(vps[i].c[1]));
-		circle(color_dst, pt, 2, Scalar(0, 255, 0));
-	}
+    // Mark the intersections of filtered lines
+    for (size_t i = 0; i < vps.size(); ++i) {
+        Point pt = Point(int(vps[i].c[0]), int(vps[i].c[1]));
+        circle(color_dst, pt, 2, Scalar(0, 255, 0));
+    }
 
-	// Step 4 & 5
-	size_t num_clusters = 0;
-	std::vector<long> cluster_belonging(vps.size(), -1);
-	std::vector<std::vector<size_t> > ind_clusters_vps;
-	std::vector<Double2> cluster_center;
-	std::vector<bool> visited(vps.size(), false);
+    // Step 4 & 5
+    size_t num_clusters = 0;
+    std::vector<long> cluster_belonging(vps.size(), -1);
+    std::vector<std::vector<size_t> > ind_clusters_vps;
+    std::vector<Double2> cluster_center;
+    std::vector<bool> visited(vps.size(), false);
 
-	for (size_t i = 0; i < vps.size(); ++i) {
-		if (visited[i])
-			continue;
-		else {
-			visited[i] = true;
-			cluster_belonging[i] = num_clusters++;
-			cluster_center.push_back(vps[i]);
-			ind_clusters_vps.push_back(std::vector<size_t>());
-			ind_clusters_vps[cluster_belonging[i]].push_back(i);
-		}
-		for (size_t j = i + 1; j < vps.size(); ++j) {
-			if (pointsDistance(cluster_center[cluster_belonging[i]], vps[j]) < CLUSTER_DIS_THRESHOLD * src.cols) {
-				// Include vps[j] into this cluster
-				visited[j] = true;
-				cluster_belonging[j] = cluster_belonging[i];
-				ind_clusters_vps[cluster_belonging[i]].push_back(j);
+    for (size_t i = 0; i < vps.size(); ++i) {
+        if (visited[i])
+            continue;
+        else {
+            visited[i] = true;
+            cluster_belonging[i] = num_clusters++;
+            cluster_center.push_back(vps[i]);
+            ind_clusters_vps.push_back(std::vector<size_t>());
+            ind_clusters_vps[cluster_belonging[i]].push_back(i);
+        }
+        for (size_t j = i + 1; j < vps.size(); ++j) {
+            if (pointsDistance(cluster_center[cluster_belonging[i]], vps[j]) < CLUSTER_DIS_THRESHOLD * src.cols) {
+                // Include vps[j] into this cluster
+                visited[j] = true;
+                cluster_belonging[j] = cluster_belonging[i];
+                ind_clusters_vps[cluster_belonging[i]].push_back(j);
 
-				// Update cluster_center
-				double x_up = 0, x_down = 0, y_up = 0, y_down = 0;
-				for (size_t k = 0; k < ind_clusters_vps[cluster_belonging[i]].size(); ++k) {
-					double x = vps[ind_clusters_vps[cluster_belonging[i]][k]].c[0];
-					double y = vps[ind_clusters_vps[cluster_belonging[i]][k]].c[1];
-					size_t ind_line1 = ind_vps_lines[ind_clusters_vps[cluster_belonging[i]][k]].s[0];
-					size_t ind_line2 = ind_vps_lines[ind_clusters_vps[cluster_belonging[i]][k]].s[1];
-					double mul_length = lineLength(lines[ind_line1]) * lineLength(lines[ind_line2]);
-					x_up += x * mul_length;
-					y_up += y * mul_length;
-					x_down += mul_length;
-					y_down += mul_length;
-				}
-				cluster_center[cluster_belonging[i]] = Double2(x_up / x_down, y_up / y_down);
-			}
-		}
-	}
+                // Update cluster_center
+                double x_up = 0, x_down = 0, y_up = 0, y_down = 0;
+                for (size_t k = 0; k < ind_clusters_vps[cluster_belonging[i]].size(); ++k) {
+                    double x = vps[ind_clusters_vps[cluster_belonging[i]][k]].c[0];
+                    double y = vps[ind_clusters_vps[cluster_belonging[i]][k]].c[1];
+                    size_t ind_line1 = ind_vps_lines[ind_clusters_vps[cluster_belonging[i]][k]].s[0];
+                    size_t ind_line2 = ind_vps_lines[ind_clusters_vps[cluster_belonging[i]][k]].s[1];
+                    double mul_length = lineLength(lines[ind_line1]) * lineLength(lines[ind_line2]);
+                    x_up += x * mul_length;
+                    y_up += y * mul_length;
+                    x_down += mul_length;
+                    y_down += mul_length;
+                }
+                cluster_center[cluster_belonging[i]] = Double2(x_up / x_down, y_up / y_down);
+            }
+        }
+    }
 
-	std::vector<size_t> cluster_sizes(num_clusters, 0);
-	for (size_t i = 0; i < vps.size(); ++i)
-		cluster_sizes[cluster_belonging[i]]++;
+    std::vector<size_t> cluster_sizes(num_clusters, 0);
+    for (size_t i = 0; i < vps.size(); ++i)
+        cluster_sizes[cluster_belonging[i]]++;
 
-	size_t max_cluster_size = 0;
-	size_t ind_largest_cluster = -1;
-	for (size_t i = 0; i < num_clusters; ++i)
-		if (cluster_sizes[i] > max_cluster_size) {
-			max_cluster_size = cluster_sizes[i];
-			ind_largest_cluster = i;
-		}
+    size_t max_cluster_size = 0;
+    size_t ind_largest_cluster = -1;
+    for (size_t i = 0; i < num_clusters; ++i)
+        if (cluster_sizes[i] > max_cluster_size) {
+            max_cluster_size = cluster_sizes[i];
+            ind_largest_cluster = i;
+        }
 
-	Double2 vp(0, 0);
+    Double2 vp(0, 0);
 
-	if (ind_largest_cluster == -1)
-		vp = Double2(-1, -1);
-	else
-		vp = cluster_center[ind_largest_cluster];
+    if (ind_largest_cluster == -1)
+        vp = Double2(-1, -1);
+    else
+        vp = cluster_center[ind_largest_cluster];
     std::cout << "(x, y) = (" << vp.c[0] << ", " << vp.c[1] << ")" << std::endl;
 
     if (vp.c[0] == -1 && vp.c[1] == -1)
